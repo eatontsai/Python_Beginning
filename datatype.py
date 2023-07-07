0f71371a740232223b5eb1d19a5678783ad8cf10
Eaton Tsai <138541479+eatontsai@users.noreply.github.com>
2023-07-07 13:26:56 +0800

    add the trial files

---
+++ datatype.py
@@ -0,0 +1,17 @@
+True
+False
+##List
+[3,4,5]
+["hello","world"]
+##Tuple
+(3,4,5)
+("hello","world")
+##Set
+{3,4,5}
+{"hello","world"}
+#dictionary
+{"apple":"A", "data":"D"}
+data_x={3,5,6}
+print(data_x)
+
+
---
+++ list.py
@@ -0,0 +1,4 @@
+grades=[12,60,15,90,70,60]
+print(grades)
+grades[1:3]=[100,100,100,100,99]
+print(grades)
